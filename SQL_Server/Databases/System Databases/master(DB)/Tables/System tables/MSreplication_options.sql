76ee1028dc9b12dd090514d8d5c8f3d5d579b1a6
HI! <jiyong5565@gmail.com>
2026-01-08 14:40:34 +0100

    Create MSreplication_options.sql

---
+++ SQL_Server/Databases/System Databases/master(DB)/Tables/System tables/MSreplication_options.sql
@@ -0,0 +1,23 @@
+USE [master]
+GO
+
+/****** Object:  Table [dbo].[spt_fallback_db]    Script Date: 1/8/2026 2:18:30 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[spt_fallback_db](
+	[xserver_name] [varchar](30) NOT NULL,
+	[xdttm_ins] [datetime] NOT NULL,
+	[xdttm_last_ins_upd] [datetime] NOT NULL,
+	[xfallback_dbid] [smallint] NULL,
+	[name] [varchar](30) NOT NULL,
+	[dbid] [smallint] NOT NULL,
+	[status] [smallint] NOT NULL,
+	[version] [smallint] NOT NULL
+) ON [PRIMARY]
+GO
+
+
